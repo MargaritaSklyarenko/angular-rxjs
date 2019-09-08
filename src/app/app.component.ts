c13e9ff64b18fb19aa8c03a5dde582232fc91261
Marharyta Skliarenko <marharyta_skliarenko@epam.com>
2019-09-08 21:17:37 +0300

    Rxjs1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ngrx';
+
+  constructor() {
+    this.firstLesson();
+  }
+
+  firstLesson() {
+    const o = Observable.create((observer: Observer<string>) => {
+      observer.next('Hello');
+      observer.complete();
+    });
+
+    o.subscribe({
+      next: (value: string) => console.log('Next:', value),
+      complete: () => console.log("Complete!"),
+      error: (error) => console.log('Error', error)
+    });
+  }
 }
